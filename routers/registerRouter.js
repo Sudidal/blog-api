a9a2503251b9d1281b388b25efc91e7e325ea5e4
oxide <mohammedhaider15mo@gmail.com>
2024-09-18 13:12:33 +0300

    feat: Add form validation with express-validator Add validation for register post req using express-validator

--- routers/registerRouter.js
+++ routers/registerRouter.js
@@ -1,22 +1,32 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import { PrismaClient } from "@prisma/client";
+import { validationResult, matchedData } from "express-validator";
+import validationChains from "../validators/validationChains.js";
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
 router.get("/", (req, res, next) => {});
-router.post("/", async (req, res, next) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  await prisma.user.create({
-    data: {
-      username: req.body.username,
-      password: hashedPassword,
-      email: req.body.email,
-      role: "ADMIN",
-    },
-  });
-  res.redirect("/login");
-});
+router.post("/", [
+  validationChains.registerValidationChain(),
+  async (req, res, next) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      console.log(validationErrors);
+      return next();
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: hashedPassword,
+        email: req.body.email,
+        role: "ADMIN",
+      },
+    });
+    res.redirect("/login");
+  },
+]);
 
 export { router as registerRouter };
---
+++ validators/validationChains.js
@@ -0,0 +1,44 @@
+import { body } from "express-validator";
+import customValidators from "./customValidators.js";
+import validationVars from "./validationVars.js";
+
+class ValidationChains {
+  constructor() {}
+
+  registerValidationChain = () => [
+    body("username")
+      .isString()
+      .trim()
+      .isLength({
+        min: validationVars.username_min,
+        max: validationVars.username_max,
+      })
+      .withMessage(
+        `Username must be between ${validationVars.username_min} and ${validationVars.username_max} characters`
+      )
+      .bail()
+      .custom(customValidators.isUsernameNotUsed)
+      .withMessage("Username already in use"),
+    body("email")
+      .isEmail()
+      .trim()
+      .withMessage("Please enter a valid E-mail address")
+      .bail()
+      .custom(customValidators.isEmailNotUsed)
+      .withMessage("E-mail already in use"),
+    body("password")
+      .isString()
+      .trim()
+      .notEmpty()
+      .withMessage("Please enter a password"),
+    body("confirm_password")
+      .isString()
+      .trim()
+      .notEmpty()
+      .custom(customValidators.isPasswordsMatch)
+      .withMessage("Passwords do not match"),
+  ];
+}
+
+const validationChains = new ValidationChains();
+export default validationChains;
