bbcdf6bcffb2b90e807e98a77bfb6982c00c9df0
oxide <mohammedhaider15mo@gmail.com>
2024-09-19 14:09:52 +0300

    Feat: make comment API routes simpler Remove unnecessary :postId in some comment specific routes

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -15,7 +15,8 @@ router.delete("/:postId", postsController.postsDelete);
 router.get("/:postId/comments", postsController.commentsGet);
 router.post("/:postId/comments", postsController.commentsPost);
 
-router.put("/:postId/comments/:commentId", postsController.commentsPut);
-router.delete("/:postId/comments/:commentId", postsController.commentsDelete);
+router.get("/comments/:commentId", postsController.commentsGetOne);
+router.put("/comments/:commentId", postsController.commentsPut);
+router.delete("/comments/:commentId", postsController.commentsDelete);
 
 export { router as postsRouter };
