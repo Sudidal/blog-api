e64b21db73526a4f0076f5d2f762513e1f2246d3
oxide <mohammedhaider15mo@gmail.com>
2024-09-24 15:41:45 +0300

    Chore: that stupid testing line again

--- routers/registerRouter.js
+++ routers/registerRouter.js
@@ -4,8 +4,5 @@ import registerController from "../controllers/registerController.js";
 const router = express.Router();
 
 router.post("/", registerController.registerPost);
-router.get("/", (req, res, next) => {
-  throw new Error("fuuuck");
-});
 
 export { router as registerRouter };
