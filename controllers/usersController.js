79b60c8bdd8a43856afdfa2cffb9edb541b488c6
oxide <mohammedhaider15mo@gmail.com>
2024-09-24 08:04:19 +0300

    Refactor: remove unnessecary jwt verifying

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import process from "node:process";
-import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -17,15 +15,9 @@ class UsersController {
     res.json({ users });
   }
   async usersGetMe(req, res, next) {
-    if (!req.headers.authorization) {
-      res.status(404).json({ message: "No authorization header provided" });
-    }
-    const token = req.headers.authorization.split(" ")[1];
-    const userId = jwt.verify(token, process.env.JWT_SECRET).id;
-
     const user = await prisma.user.findUnique({
       where: {
-        id: userId,
+        id: req.user.id,
       },
       select: {
         username: true,
